330aaaff03acc42af6d87844e87f73a3bd6c0fc3
Hayato0812 <hayato08120904@gmail.com>
2019-11-04 11:23:54 +0000

    エラーのところまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
-    @user = User.find(params[:id])
+    @user = users(:michael)
+    @other_user = users(:archer)
   end
   
   def new
@@ -17,6 +20,32 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'  
+    end
+  end
+  
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
   
   private
