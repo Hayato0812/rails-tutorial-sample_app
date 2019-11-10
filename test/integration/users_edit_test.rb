a25226b68e2567a0dfa1b3c04f7df597b26618c4
Hayato0812 <hayato08120904@gmail.com>
2019-11-10 21:33:21 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,9 +6,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  def setuprails test
-    @user = users(:michael)
-  end
 
   test "unsuccessful edit" do
     log_in_as(@user)
@@ -22,10 +19,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
   
-  test "successful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
      patch user_path(@user), params: { user: { name:  name,
